c498ecd8e6a5611a8bcbe589f536d0984671a1b3
Dhruv Kundu <81622271+dhruv-kundu14@users.noreply.github.com>
2023-05-22 09:53:00 +0530

    conn com

--- src/Oracleconn.java
+++ src/Oracleconn.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.util.Scanner;
 import java.sql.DriverManager;
 //import java.sql.SQLException;
 import java.sql.*;
@@ -9,6 +10,7 @@ public class Oracleconn {
 		// TODO Auto-generated method stub
 		try 
 		{
+			Scanner sc = new Scanner(System.in);
 		    Class.forName("oracle.jdbc.OracleDriver");
 		
 		
@@ -18,17 +20,49 @@ public class Oracleconn {
 		
 		Connection conn =  DriverManager.getConnection(url, username, password);
 		System.out.println("connected");
-		System.out.println("Creating statement...");
+		String q = "insert into connectdb values(?,?,?)";
+		PreparedStatement pst = conn.prepareStatement(q);
+		char ch=0;
+		String c = null;
+		int n=0;
+		do {
+			System.out.println("Enter Roll no");
+			int r = sc.nextInt();
+			System.out.println("Enter Name");
+			String name = sc.next();
+			System.out.println("Enter marks");
+			double m = sc.nextDouble();
+			pst.setInt(1, r);
+			pst.setString(2, name);
+			pst.setDouble(3, m);
+			n=pst.executeUpdate();
+			System.out.println("Do you want to continue");
+			c  = sc.next();
+			ch = c.charAt(0);
+			}
+		while(ch=='y' || ch=='Y');
+		
+		if(n>0) {
+			System.out.println("Data inserted");
+		}
+		else {
+			System.out.println("Not Inserted");
+		}
+		
+/*		System.out.println("Creating statement...");
 		Statement stmt = conn.createStatement();
 		System.out.println("Data Insertion StaRTED...");
 		String sql="insert into connectdb values (19,'Dhruv Kundu', '2201139')";
 		stmt.executeUpdate (sql); 
 		System.out.println("dATA Inserted Successfully...");
+		*/
 		
 		}
 		catch(Exception ex) {
 			System.out.println(ex);
 		}
+		
+		
 		
 		
 
